731dcc450e4beab7db45b8da0ec67b8aedea843f
BenoitGoethals <benoit.goethals@gmail.com>
2018-03-30 18:43:24 +0200

    init

---
+++ DelegateProj/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace DelegateProj
+{
+    /**
+    Points to Remember :
+    Delegate is a function pointer. It is reference type data type.
+    Syntax: public delegate void <function name>(<parameters>)
+    A method that is going to assign to delegate must have same signature as delegate.
+    Delegates can be invoke like a normal function or Invoke() method.
+    Multiple methods can be assigned to the delegate using "+" operator. It is called multicast delegate.
+    **/
+    public delegate int Calculate(params int[]nums);
+
+    public class Calculator
+    {
+        private Calculate _calculate;
+
+
+        public void SetCalulate(Calculate calculate)
+        {
+            this._calculate += calculate;
+        }
+
+
+        public int GetResult(params int[] nums)
+        {
+            return _calculate(nums);
+        }
+
+
+        public static int PlusCal(params int[] nums)
+        {
+
+            return nums.Sum();
+        }
+
+        public static int PlusMultiPly(params int[] nums)
+        {
+
+       
+            int ret = 0;
+            foreach (var vNum in nums)
+            {
+                ret *= vNum;
+            }
+            return ret;
+        }
+
+    }
+}
---
+++ DelegateProj/Starter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProj
+{
+
+    public class Starter
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator=new Calculator();
+            calculator.SetCalulate(Calculator.PlusMultiPly);
+            calculator.SetCalulate(Calculator.PlusCal);
+            int res = calculator.GetResult(12,12,12);
+            Console.WriteLine(res);
+            calculator.SetCalulate(k => k.Max());
+            Console.WriteLine(res);
+
+           
+            Console.WriteLine(calculator.GetResult(12, 12, 12));
+
+            Console.ReadLine();
+        }
+    }
+}
